83e7dba6b742488d0f662b51a873cfa1b40547d1
mjschleckser <mjschleckser@gmail.com>
2016-11-02 15:43:23 -0400

    Added Drawing class

--- AndroidDoodle/app/src/main/java/edu/umd/cs/androiddoodle/MainActivity.java
+++ AndroidDoodle/app/src/main/java/edu/umd/cs/androiddoodle/MainActivity.java
@@ -3,11 +3,17 @@ package edu.umd.cs.androiddoodle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
         setContentView(R.layout.activity_main);
     }
 }
